b1a0a55b312daa9660a05b43f780fb8917b7a4c8
b-steiner <bernhard.steiner@tuwien.ac.at>
2024-01-24 17:15:55 +0100

    Added example on how to create a new project

--- src/common/import_simultan.py
+++ src/common/import_simultan.py
@@ -8,6 +8,10 @@ import os
 simultan_sdk_var = os.environ.get('SIMULTAN_SDK_DIR')
 clr.AddReference(simultan_sdk_var+'\\SIMULTAN.dll')    # the dll
 
+#Load additional .NET assemblies
+clr.AddReference("System.Security.Cryptography")
+
+# Import SIMULTAN Namespaces
 from SIMULTAN.Data import *
 from SIMULTAN.Data.Geometry import *
 from SIMULTAN.Data.Components import *
@@ -15,5 +19,10 @@ from SIMULTAN.Data.Users import *
 from SIMULTAN.Projects import *
 from SIMULTAN.Utils import *
 from SIMULTAN.Serializer.Projects import *
+
+# Import useful .NET namespaces
+from System import *
 from System.IO import *
 from System.Security import *
+from System.Security.Cryptography import *
+from System.Text import *
