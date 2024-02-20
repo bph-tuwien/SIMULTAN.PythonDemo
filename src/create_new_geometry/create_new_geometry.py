b3db0e05c2e1cf9ae31f8859849e5577799eb2ad
Jenö <jenoe.pazmandi@tuwien.ac.at>
2024-02-20 09:49:59 +0100

    accessing adjacent buildings

--- src/create_new_geometry/create_new_geometry.py
+++ src/create_new_geometry/create_new_geometry.py
@@ -6,8 +6,17 @@ from SIMULTAN.Serializer.SimGeo import *
 def create_new_geometry(project, projectData, name= "New Geometry"):
 	resourceFile = project.AddEmptyGeometryResource(project.ProjectUnpackFolder,
 		name,  f'{0} ({1})', project.AllProjectDataManagers.DispatcherTimerFactory)
-		# Load the geometry model
-	model_to_work_with = SimGeoIO.Load(resourceFile, projectData, None)
+		
+	model_to_work_with = SimGeoIO.Load(resourceFile, projectData, None) # Load the geometry model
+
+
+	layer =  Layer(model_to_work_with, "New Layer") # Adding a laer to the empty model
+	model_to_work_with.Layers.Add(layer)
+
+
+	position = SimPoint3D(0,0,0)
+	vertex = Vertex(layer, "New Vertex", position)
+
 	return model_to_work_with
 
 
